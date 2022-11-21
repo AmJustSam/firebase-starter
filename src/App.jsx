7c7fe0f1737f0f0f15b7a9119de74a3cca54e27f
Usama Tahir <hellousama@protonmail.com>
2022-11-21 21:09:09 +0500

    Improved code

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,9 @@
-import { collection, doc, getDoc, getDocs } from "firebase/firestore";
-import { useEffect, useState } from "react";
 import db from "./utils/firebase";
 
 function App() {
-  const [user, setUser] = useState({});
-
-  useEffect(() => {
-    getDocuments();
-  }, [])
-
-  const getDocuments = async () => {
-    const customersRef = collection(db, "users");
-    const docRef = doc(customersRef, "pLd4zfVRvJO44XJFF5vE");       // const docRef = doc(db, "users", "pLd4zfVRvJO44XJFF5vE");
-        
-    const docRes = await getDoc(docRef);
-
-    setUser(
-      {id: docRes.id, ...docRes.data()}
-    );
-  }
-
   return (
     <div>
-      <div>
-         <h1>{user.name}</h1>
-         <p>{user.age}</p>
-       </div>
+      <h1>Hello World</h1>
     </div>
   )
 }
